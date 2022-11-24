3317a9797b4359559a7f575e123e538e61fbbd0a
Folltoshe <folltoshe@foxmail.com>
2022-11-24 22:43:56 +0800

    更新

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -43,6 +43,7 @@ export default hopeTheme({
   // },
 
   plugins: {
+    photoSwipe: undefined,
     // If you don't need comment feature, you can remove following option
     // The following config is for demo ONLY, if you need comment feature, please generate and use your own config, see comment plugin documentation for details.
     // To avoid disturbing the theme developer and consuming his resources, please DO NOT use the following config directly in your production environment!!!!!
